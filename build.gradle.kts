cb3ce12957a94b86cc0cbd6a571f9651812d4357
Benjamin Gudehus <hastebrot@gmail.com>
2017-09-11 01:28:01 +0200

    (chore) Gradle: Update to Gradle 4.2-rc-1 and JUnit 5.0.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
+// gradle wrapper --gradle-version 4.2-rc-1 --distribution-type all
+
 val kotlinVersion by project
+val junitJupiterVersion by project
+val junitPlatformVersion by project
 
 plugins {
     kotlin("jvm", "1.1.4-3")
@@ -15,4 +19,13 @@ dependencies {
     compile("com.canoo.dolphin-platform:dolphin-platform-remoting-client-javafx:0.18.0")
     compile("com.canoo.dolphin-platform:dolphin-platform-remoting-server-spring:0.18.0")
 //    compile("org.slf4j:slf4j-simple:1.7.25")
+}
+
+dependencies {
+    testCompile(kotlin("test", "$kotlinVersion"))
+
+    // junit platform.
+    testCompile("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+    testRuntime("org.junit.platform:junit-platform-launcher:$junitPlatformVersion")
 }
